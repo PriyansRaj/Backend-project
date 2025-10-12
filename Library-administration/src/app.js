c039d29115d9856291a19740f812039eea3ffad8
= <=>
2025-10-12 21:57:47 +0530

    Added book route

--- Library-administration/src/app.js
+++ Library-administration/src/app.js
@@ -1,10 +1,19 @@
 import express from "express"
 import userRoutes from "./routes/user.routes.js";
 import adminRoutes from "./routes/admin.routes.js"
+import bookRoutes from "./routes/book.routes.js"
 import cookieParser from "cookie-parser"
+import helmet from "helmet"
+import cors from "cors"
 const app = express();
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+    origin:"*",
+    credentials:true
+}))
+app.use(helmet())
 app.use("/api/v1/users",userRoutes)
 app.use("/api/v1/admin",adminRoutes)
+app.use("/api/v1/books",bookRoutes)
 export default app;
